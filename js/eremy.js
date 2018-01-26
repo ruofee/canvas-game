e68bc9dbc189e082076fcb3c2ad8258fd0052b31
ruofee <768047505@qq.com>
2018-01-26 21:51:32 +0800

    确定游戏名为flappy bird的canvas小游戏

--- js/eremy.js
+++
@@ -1,9 +0,0 @@
-let eremy = {
-	test: {
-		blood: 1,
-		width: 0,
-		height: 0,
-		r: 30,
-		color: '#red'
-	}
-}
--- js/ready.js
+++
@@ -1,4 +0,0 @@
-function drawReady(ctx1, ctx2, width, height) {
-	ctx1.clearRect(0,0,width,height)
-	drawGoBox(ctx1, width, height)
-}
